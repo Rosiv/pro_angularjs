6593e532f764751333e5cd787128f376a34f2869
rosiv <rosiu@outlook.com>
2016-04-05 20:25:55 +0200

    chapter 1

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-var connect = require('connect');
- 
-connect.createServer(
-    connect.static("../angularjs")
- 
-).listen(5000);
+var connect = require('connect'),
+serveStatic = require('serve-static');
+
+var app = connect();
+
+app.use(serveStatic("./angularjs"));
+app.listen(5000);
